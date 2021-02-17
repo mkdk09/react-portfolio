6acc8467625b3e05f10eba3d472120d53187ff36
mkdk09 <mkdk099@gmail.com>
2021-02-17 16:41:40 +0900

    自己紹介（About）を作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar'
 import MyAvatar from './components/MyAvatar'
+import About from './components/About'
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
       <MyAvatar />
+      <About />
     </>
   );
 }
