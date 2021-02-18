0ccf88793fca3ad65e3941d844dcd67ee52c5303
mkdk09 <mkdk099@gmail.com>
2021-02-18 14:40:47 +0900

    連絡先（Contact）を作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MyAvatar from './components/MyAvatar'
 import About from './components/About'
 import Skills from './components/Skills'
 import Works from './components/Works'
+import Contact from './components/Contact'
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <About />
       <Skills />
       <Works />
+      <Contact />
     </>
   );
 }
