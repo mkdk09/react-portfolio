6eeb64e09c5f38b47a9da5fa96e6bfaa3ae82e76
mkdk09 <mkdk099@gmail.com>
2021-03-20 17:26:58 +0900

    ページタイトルを修正

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,35 @@ import Skills from './components/Skills'
 import Works from './components/Works'
 import Contact from './components/Contact'
 import ScrollUp from './utils/ScrollUp'
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const App: React.FC = () => {
   return (
     <>
-      <section>
-        <Navbar />
-      </section>
-      <section>
-        <MyAvatar />
-      </section>
-      <section id='about'>
-        <About />
-      </section>
-      <section id='skills'>
-        <Skills />
-      </section>
-      <section id='works'>
-        <Works />
-      </section>
-      <section id='contact'>
-        <Contact />
-      </section>
-      <ScrollUp />
+      <HelmetProvider>
+        <Helmet>
+          <title>Portfolio</title>
+        </Helmet>
+        <section>
+          <Navbar />
+        </section>
+        <section>
+          <MyAvatar />
+        </section>
+        <section id='about'>
+          <About />
+        </section>
+        <section id='skills'>
+          <Skills />
+        </section>
+        <section id='works'>
+          <Works />
+        </section>
+        <section id='contact'>
+          <Contact />
+        </section>
+        <ScrollUp />
+      </HelmetProvider>
     </>
   );
 }
